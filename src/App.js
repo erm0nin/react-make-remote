06270656922bf723ecccc728e2d0be594beb87dd
dima <ermonindima0@gmail.com>
2024-09-18 16:27:42 +0300

    last

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Wrapper } from "./components/wrapper";
+import { Header } from "./components/header";
+import { MainSection } from "./components/main-section/main-section";
+function App() {
+  return (
+    <Wrapper >
+     <Header/>
+     <MainSection/>
+    </Wrapper>
+  );
+}
+
+export default App;
---
+++ src/components/wrapper/index.jsx
@@ -0,0 +1,11 @@
+import React, { Children } from 'react'
+
+export const Wrapper = ({children}) => {
+  return (
+    <div className='container mx-auto py-4 px-2 min-h-screen'>
+      {children}
+    </div>
+  )
+}
+
+
